f73bfa0237ec139b83b66a383bd0c3f60db88c30
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-05-24 21:38:52 +0200

    Lesson 56 Completed
    This lesson looked at implementing adding a user to a session in order to log them in, we also implemented the log out functionality along with the conditionals for when a user sees log in/out.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, g, request, url_for, session
+from flask import Flask, render_template, g, request, url_for, session, redirect
 from database import get_db
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24) # Creating the secret key using 24 random characters.
 
 @app.teardown_appcontext
 def close_db(error):
@@ -11,8 +13,12 @@ def close_db(error):
 
 @app.route('/')
 def index():
+    # Checking to see if the user is in the session:
+    user = None
+    if "user" in session:
+        user = session['user']
     
-    return render_template('home.html')
+    return render_template('home.html', user=user)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -49,7 +55,11 @@ def login():
         user_cursor = db.execute('select id, name, password from users where name = ?', [name])
         user_result = user_cursor.fetchone()
 
+        # Checking to see if the user entered password matches the password saved in the database.
         if check_password_hash(user_result['password'], password):
+
+            session['user'] = user_result['name']
+
             return "<h1>The password is correct</h1>"
         else:
             return "<h1>The password is incorrect</h1>"
@@ -81,6 +91,11 @@ def unanswered():
 def users():
     
     return render_template('users.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
